985bb042749d4f21b20f71c5c80a679823fd0d14
TBest <TBestLittleHelper@users.noreply.github.com>
2026-09-11 17:51:45 +0200

    select a random batch of puzzles, from the samples

--- src/ninja-chess/puzzle.ts
+++ src/ninja-chess/puzzle.ts
@@ -1,31 +1,25 @@
 import type { Puzzle, CupName } from './types';
 
 export const PuzzleBatchSize = 30;
+const BatchesPerCup = 3;
 
-const puzzleCache: Partial<Record<CupName, Puzzle[]>> = {};
+const puzzleCache: Partial<Record<string, Puzzle[]>> = {};
 
-async function loadPuzzleData(cup: CupName): Promise<Puzzle[]> {
-	switch (cup) {
-		case 'camel':
-			return (await import('../assets/camel.json')).default.puzzles as Puzzle[];
-		case 'frog':
-			return (await import('../assets/frog.json')).default.puzzles as Puzzle[];
-		case 'spider':
-			return (await import('../assets/spider.json')).default.puzzles as Puzzle[];
-		case 'rhino':
-			return (await import('../assets/rhino.json')).default.puzzles as Puzzle[];
-		case 'fish':
-		default:
-			return (await import('../assets/fish.json')).default.puzzles as Puzzle[];
+async function loadPuzzleData(cup: CupName, batch: number): Promise<Puzzle[]> {
+	const cacheKey = `${cup}-${batch}`;
+	if (puzzleCache[cacheKey]) {
+		return puzzleCache[cacheKey]!;
 	}
+
+	const puzzles = (await import(`../assets/${cup}-${batch}.json`)).default.puzzles as Puzzle[];
+
+	puzzleCache[cacheKey] = puzzles;
+	return puzzles;
 }
 
 export async function getPuzzleBatch(cup: CupName = 'fish') {
-	if (!puzzleCache[cup]) {
-		puzzleCache[cup] = await loadPuzzleData(cup);
-	}
-
-	const puzzleDatabase = puzzleCache[cup]!;
+	const batch = Math.floor(Math.random() * BatchesPerCup) + 1;
+	const puzzleDatabase = await loadPuzzleData(cup, batch);
 	const selected = new Set<number>();
 	const puzzleDatabaseLength = puzzleDatabase.length;
 
